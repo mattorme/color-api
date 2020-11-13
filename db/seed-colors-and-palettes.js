83b3939292746b07bed93def8a60060071ec4bab
Matt <ormematthewj@gmail.com>
2020-11-14 10:35:48 +1100

    palletes page

--- db/seed-colors-and-palettes.js
+++ db/seed-colors-and-palettes.js
@@ -1,35 +1,21 @@
+var colors = require("./colors-list");
+const { Pool } = require("pg");
+const pool = new Pool({ database: "colors_api", password: "password" });
 
-var colors = require ('./colors-list');
-const {Pool} = require('pg')
-const pool = new Pool({database: 'colors_api', password: 'password'}
+colors = colors.split("\n");
+var pallete = [];
 
+colors.forEach((item) => {
+  item = item.split("\t");
+  pallete.push(item[3]);
+  if (pallete.length == 5) {
+    pool.query(
+      `insert into palettes (primary_color_hex, secondary_color_hex, tertiary_color_hex, quaternary_color_hex, quinary_color_hex) values ('${pallete[0]}', '${pallete[1]}','${pallete[2]}', '${pallete[3]}','${pallete[4]}');`
+    );
+    pallete = [];
+  }
 
-colors = colors.split('\n')
-var pallete = []
-
-colors.forEach(item => {
-    item = item.split("\t")
-    pallete.push(item[3])
-    if (pallete.length == 5) {
-        pool.query(`insert into palettes (primary_color_hex, secondary_color_hex, tertiary_color_hex, quaternary_color_hex, quinary_color_hex) values ('${
-            pallete[0]
-        }', '${
-            pallete[1]
-        }','${
-            pallete[2]
-        }', '${
-            pallete[3]
-        }','${
-            pallete[4]
-        }');`)
-        pallete = []
-    }
-    
-    pool.query(`insert into colors (color_name, color_hex) values ('${
-        item[0]
-    }', '${
-        item[3]
-    }');`)
-
-
+  pool.query(
+    `insert into colors (color_name, color_hex) values ('${item[0]}', '${item[3]}');`
+  );
 });
