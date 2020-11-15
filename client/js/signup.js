a03fae4dcc33c288ee284e12e82080896f2d8245
Naveen <navi.mehta@gmail.com>
2020-11-15 15:13:43 +1100

    added login functionality

--- client/js/signup.js
+++ client/js/signup.js
@@ -5,6 +5,10 @@ let signupEmail = document.querySelector(".signup-email")
 let signupPassword = document.querySelector(".signup-password")
 let signupmsg = document.querySelector(".signup-msg")
 
+let loginBtn = document.querySelector(".login-btn")
+let loginEmail = document.querySelector(".login-email")
+let loginPassword = document.querySelector(".login-password")
+
 
 function handleReturn(res) {
     if (res.data.message === "ok") {
@@ -19,16 +23,14 @@ signupBtn.addEventListener('click', (event) => {
         .then(handleReturn)
 })
 
-// let loginBtn = document.querySelector(".login-page-btn")
-// let loginEmail = document.querySelector("")
-// let loginPassword = document.querySelector("")
-
-// loginBtn.addEventListener('click', (event) => {
-//     event.preventDefault() 
-//     axios.post(`/login`, {  email: loginEmail.value, password: loginPassword.value })
-//         .then((res) => {
-//             //redirect to logged in home page
-//             window.location = "/colors/1";
-//         })
-// })
-
+loginBtn.addEventListener('click', (event) => {
+    event.preventDefault() 
+    axios.post(`/login`, {  email: loginEmail.value, password: loginPassword.value })
+        .then((res) => {
+            if(res.data.login) { 
+                window.location = `/colors/${res.data.user.id}`;
+            } else { 
+                console.log("login failed")
+            }
+        })
+})
