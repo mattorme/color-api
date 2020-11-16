9547e8f2f26cb5e8dfffae0ad0780762652e7d23
Stephen <stephen@Lisas-Air.modem>
2020-11-16 15:04:25 +1100

    added functioinality of save btn

--- client/js/save.js
+++ client/js/save.js
@@ -1,18 +1,19 @@
 const saveBtn = document.querySelector(".save-btn");
-// const primaryHex = document.querySelector('.primary-hex')
-// const secondaryHex = document.querySelector('.secondary-hex')
-// const tertiaryHex = document.querySelector('.tertiary-hex')
-// const quaternaryHex = document.querySelector('.quaternary-hex')
-// const quinaryHex = document.querySelector('.quinary-hex')
 
 saveBtn.addEventListener('click', function() { 
     let pallete = {
-        user_id: `${saveBtn.dataset.userid}`, 
+        user_id: saveBtn.dataset.userid, 
         primary_color_hex: `${primaryHex.textContent}`, 
         secondary_color_hex: `${secondaryHex.textContent}`, 
         tertiary_color_hex: `${tertiaryHex.textContent}`, 
         quaternary_color_hex: `${quaternaryHex.textContent}`, 
         quinary_color_hex: `${quinaryHex.textContent}`, 
     }
-    console.log(pallete)
+
+    axios.post(`/api/palettes`, pallete)
+        .then((res) => {
+            //Can add something here that lets you know a palette has been saved
+            saveBtn.style.background = "yellow"
+           console.log(res)
+        })
 })
